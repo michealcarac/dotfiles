6676c796063a4bad9a922ce4881c5758bc4648ca
Empyrea <sixpennymovie@gmail.com>
2023-05-08 23:24:00 +0000

    Updated Fonts

--- scripts/.local/bin/installFonts.sh
+++ scripts/.local/bin/installFonts.sh
@@ -2,6 +2,7 @@
 # Installs the following fonts:
 # -> Awesome Fonts 6
 # -> Awesome Fonts 4
+# -> Nerd Fonts
 
 # Vars
 AWESOME_FONTS6="https://use.fontawesome.com/releases/v6.4.0/fontawesome-free-6.4.0-desktop"
@@ -27,6 +28,11 @@ cp $AWESOME_FONTS6_FILE/otfs/* ~/.fonts/
 wget $AWESOME_FONTS4.zip
 unzip $AWESOME_FONTS4_FILE.zip
 cp $AWESOME_FONTS4_FILE/fonts/*.otf ~/.fonts/
+
+# Download Nerd Fonts from Arch Community
+# Maybe in future make this not need pacman..
+sudo pacman -S ttf-nerd-fonts-symbols 
+
 
 # Refresh Font Cache
 fc-cache -f -v
